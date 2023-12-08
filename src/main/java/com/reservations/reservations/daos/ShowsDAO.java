1e41349a43f2fcac9bd44b8dd6a6a5e7b7692aa3
Ibrahima_Diallo <dickoya68@gmail.com>
2023-12-08 11:07:10 +0100

    ShowsDAO (Data Access Object)faire le lien entre ta couche métier et la couche de persistance

---
+++ src/main/java/com/reservations/reservations/daos/ShowsDAO.java
@@ -0,0 +1,28 @@
+package com.reservations.reservations.daos;
+
+
+import com.reservations.reservations.models.Show;
+import com.reservations.reservations.repositories.ShowRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ShowsDAO {
+    private ShowRepository showRepository;
+    public Show save(Show show){return showRepository.save(show);}
+
+    public Show findById(Long id) {
+        return showRepository.findById(id).orElse(null);
+    }
+
+    public Iterable<Show> findAll() {
+        return showRepository.findAll();
+    }
+
+    public void delete(Show show) {
+        showRepository.delete(show);
+    }
+
+    public void deleteById(Long id) {
+        showRepository.deleteById(id);
+    }
+}
